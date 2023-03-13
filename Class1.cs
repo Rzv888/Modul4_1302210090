f8695dd00bb2761b8d026f1b48317a43fbef4eb8
PC1-IFLAB <PC1-IFLAB>
2023-03-13 07:48:55 +0700

    menambahkan Class Kode Buah

---
+++ Class1.cs
@@ -0,0 +1,31 @@
+﻿public class KodeBuah
+{
+    public string getKodeBuah(string Buah) {
+        switch (Buah)
+        {
+            case "Apel":
+                return "A00";
+            case "Aprikot":
+                return "B00";
+            case "Alpukat":
+                return "C00";
+            case "Pisang":
+                return "D00";
+            case "Paprika":
+                return "E00";
+            case "Blackberry":
+                return "F00";
+            case "Ceri":
+                return "H00";
+            case "Kelapa":
+                return "I00";
+            case "Jagung":
+                return "J00";
+            default:
+                return "Kode Buah Tidak Ada";
+
+        }
+    }
+}
+
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class MainClass
+{
+    public static void Main(string[] args)
+    {
+        KodeBuah KodeBuah = new KodeBuah();
+        string NamaBuah = "Paprika";
+        string KodeNamaBuah = KodeBuah.getKodeBuah(NamaBuah);
+        Console.WriteLine($"Kode Nama Buah {NamaBuah}:{KodeNamaBuah}");
+    }
+}
